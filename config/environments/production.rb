0afad9eb4287418d60240fbe1da0ef30d2b468aa
Tobias Schmidt <t.schmidt@rubidat.de>
2015-05-28 13:38:32 +0200

    mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { :host => 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "mail.zedat.fu-berlin.de",
+    port: 587,
+    domain: 'localhost',
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: Rails.application.secrets.MAIL_USERNAME,
+    password: Rails.application.secrets.MAIL_PASSWORD
+  }
 end
